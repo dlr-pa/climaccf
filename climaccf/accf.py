fixing the calculation of missing variables from alternative variables: relative humidity over ice and potential vorticity

climaccf/accf.py
@@ -264,9 +264,9 @@ class GeTaCCFs(object):
         # CO2:
         attrs_CO2 = {'unit': 'K kg(fuel)**-1', 'long_name': 'algorithmic climate change function of CO2', 'short_name': 'aCCF of CO2'}
         if self.aCCF_Version == 'V1.0':
-            self.aCCF_CO2 =  6.94 * 1e-16 * np.ones(self.t.shape)*self.sf['CO2']/self.base['CO2']   # P-ATR20-CO2 [K/kg(fuel)]
+            self.aCCF_CO2 =  7.48 * 1e-16 * np.ones(self.t.shape)*self.sf['CO2']/self.base['CO2']   # P-ATR20-CO2 [K/kg(fuel)]
         elif self.aCCF_Version  == 'V1.0A':
-            self.aCCF_CO2 =  6.94 * 1e-16 * np.ones(self.t.shape)*self.sf['CO2']/self.eg['CO2'] 
+            self.aCCF_CO2 =  7.48 * 1e-16 * np.ones(self.t.shape)*self.sf['CO2']/self.eg['CO2'] 
         else:
             raise ValueError("Currently, the versions of aCCFs reported in Yin et al. (2023) ('V1.0') and 'Matthes et al. (2022) ('V1.0A) have been implemented; thus, the correct option is confg['aCCF-V'] = 'V1.0' or 'V1.0A'.")        
         self.aCCF_CO2 = convert_accf('CO2', self.aCCF_CO2, confg)

climaccf/calc_altrv_vars.py
@@ -7,7 +7,7 @@ from metpy.units import units
 import xarray as xr
 
 
-def get_pvu(ds):
+def get_pvu_ens(ds):
     """
     Calculates potential vorticity [in PVU] from meteorological variables pressure, temperature and x and y component of the wind using MetPy (https://www.unidata.ucar.edu/software/metpy/).
 
@@ -67,8 +67,72 @@ def get_pvu(ds):
                               ds['level'].values[in_lvl] * np.ones(shape_gridlat),
                               ds['level'].values[below] * np.ones(shape_gridlat)]) * units.mbar
 
-                PV = calc.potential_vorticity_baroclinic(PT, p, U, V, dx, dy, lat * units.degree)
+                PV = calc.potential_vorticity_baroclinic(PT, p, U, V, dx, dy, lat * units.degree, longitude=lon * units.degree)
                 pvu[i, j, k, :, :, :] = PV.magnitude
+
+    return pvu
+
+
+def get_pvu_det(ds):
+    """
+    Calculates potential vorticity [in PVU] from meteorological variables pressure, temperature and x and y component of the wind using MetPy (https://www.unidata.ucar.edu/software/metpy/).
+
+    :param ds: Dataset opened with xarray.
+    :type ds: Dataset
+
+    :returns PVU: potential vorticity [in PVU]
+    :rtype: numpy.ndarray
+    """
+    Levels = ds['level'].values
+    Ava_levels = Levels[1:-1]
+    pvu = np.zeros((len(ds['time'].values), len(Ava_levels), 3, len(ds['latitude'].values), len(ds['longitude'].values)))
+
+    for k in range(len(Ava_levels)):
+        level = Ava_levels[k]
+        in_lvl = np.where(level == ds['level'].values)
+        above = in_lvl[0][0] - 1
+        below = in_lvl[0][0] + 1
+
+        Tna = ds['t'].values[:, above, :, :]
+        Una = ds['u'].values[:, above, :, :]
+        Vna = ds['v'].values[:, above, :, :]
+
+        Tn = ds['t'].values[:, in_lvl[0][0], :, :]
+        Un = ds['u'].values[:, in_lvl[0][0], :, :]
+        Vn = ds['v'].values[:, in_lvl[0][0], :, :]
+
+        Tnb = ds['t'].values[:, below, :, :]
+        Unb = ds['u'].values[:, below, :, :]
+        Vnb = ds['v'].values[:, below, :, :]
+
+        p_above_u = ds['level'].values[above]  * units.mbar
+        p_lvl_u   = ds['level'].values[in_lvl] * units.mbar
+        p_below_u = ds['level'].values[below]  * units.mbar
+        shape_gridlat = Un[0, :, :].shape
+        shape_gridlon = (Un[0, :, :]).T.shape
+
+        lat = (np.ones(shape_gridlon) * ds['u'].latitude.values).T
+        lon = np.ones(shape_gridlat) * ds['u'].longitude.values
+
+        R = 6371000  # disregard h
+        dx = (lon[0, 1] - lon[0, 0]) * np.pi / 180 * R * units.meter
+        dy = (lat[0, 0] - lat[1, 0]) * np.pi / 180 * R * units.meter
+
+        for i in range(0, len(ds['time'].values)):
+            PT_above_u = calc.potential_temperature(p_above_u, Tna[i, :, :] * units.kelvin)
+            PT_lvl_u = calc.potential_temperature(p_lvl_u, Tn[i, :, :] * units.kelvin)
+            PT_below_u = calc.potential_temperature(p_below_u, Tnb[i, :, :] * units.kelvin)
+
+            # Assemble inputs to PV function from higher in the atmosphere to closer to the surface
+            PT = np.array([PT_above_u.magnitude, PT_lvl_u.magnitude, PT_below_u.magnitude]) * units.kelvin
+            U = np.array([Una[i, :, :], Un[i, :, :], Unb[i, :, :]]) * units.meter / units.second
+            V = np.array([Vna[i, :, :], Vn[i, :, :], Vnb[i, :, :]]) * units.meter / units.second
+            p = np.array([ds['level'].values[above] * np.ones(shape_gridlat),
+                            ds['level'].values[in_lvl] * np.ones(shape_gridlat),
+                            ds['level'].values[below] * np.ones(shape_gridlat)]) * units.mbar
+
+            PV = calc.potential_vorticity_baroclinic(PT, p, U, V, dx, dy, lat * units.degree)
+            pvu[i, k, :, :, :] = PV.magnitude
 
     return pvu
 

climaccf/weather_store.py
@@ -6,6 +6,9 @@ from datetime import datetime
 from scipy.signal import convolve2d, convolve
 from climaccf.extract_data import *
 from climaccf.processing_surf_vars import *
+from climaccf.calc_altrv_vars import get_pvu_det, get_pvu_ens 
+from climaccf.contrail import get_rw_from_specific_hum, get_r_ice_from_r_w
+import copy
 
 
 def get_bound_indexes(arr, bounds, verbose=False):
@@ -280,9 +283,43 @@ class WeatherStore(WeatherStore_):
         if self.cfg['predecimate']:
             self.axes['latitude'] = self.axes['latitude'][::2 ** self.downsample_steps]
             self.axes['longitude'] = self.axes['longitude'][::2 ** self.downsample_steps]
+            
+        self.ds_xr = xr.Dataset(self.var_xr, self.axes)
+        ### Calculation of PV from alternative variables, i.e., temperature and components of wind
+        if 'pv' not in self.pre_variable_names and 't' in self.pre_variable_names and 'u' in self.pre_variable_names and 'v' in self.pre_variable_names:
+            pv_dict = {}
+            if inf_coordinates['logic_coordinate']['member']:
+                pv = get_pvu_ens(self.ds_xr)[:,:,:,1,:,:]
+                pv_dict['pv'] = (tuple(self.coordinate_names), pv, {'units': '"K m**2 kg**-1 s**-1"', 'long_name': 'Potential vorticity', 'NOTE': 'calculated from alternative variables'})         
+            else:
+                pv = get_pvu_det(self.ds_xr)[:,:,1,:,:]
+                pv_dict['pv'] = (tuple(self.coordinate_names), pv, {'units': '"K m**2 kg**-1 s**-1"', 'long_name': 'Potential vorticity', 'NOTE': 'calculated from alternative variables'})                
+            # adapt dimenstions to calculation of pv, i.e., first and last pressure levels need to be removed. 
+            self.axes ['level'] = self.axes['level'][1:-1]       
+            ds_xr_pv = xr.Dataset(pv_dict, self.axes)      
+            self.ds_xr = self.ds_xr.isel(level=slice(1, -1))
+            self.ds_xr = xr.merge([self.ds_xr, ds_xr_pv])
+            ds_xr_pv.close()
+            self.var_xr = {var_name: self.ds_xr[var_name] for var_name in self.ds_xr.data_vars}      
+        
+        
+        ### Calculation of relative humidity over ice from alternative variables, i.e., temperature and specific humidity
+        if 'r' not in self.pre_variable_names and 't' in self.pre_variable_names and 'q' in self.pre_variable_names:
+            r_dict = {}
+            r_w = get_rw_from_specific_hum(self.ds_xr, inf_coordinates['logic_coordinate']['member'])     
+            r_i = get_r_ice_from_r_w (r_w, self.ds_xr.t.values)   
+            r_dict['r'] =  (tuple(self.coordinate_names), r_i, {'units': '[-]', 'long_name': 'Relative humidity', 'standard_name': 'relative_humidity'})       
+            ds_xr_r = xr.Dataset(r_dict, self.axes)      
+            self.ds_xr = xr.merge([self.ds_xr, ds_xr_r])
+            ds_xr_r.close()
+            self.var_xr = {var_name: self.ds_xr[var_name] for var_name in self.ds_xr.data_vars}        
+
+            
         if self.cfg['save_as_xr']:
-            ds_xr = xr.Dataset(self.var_xr, self.axes)
-            #ds_xr.to_netcdf('/Users/abolfazlsimorgh/Desktop/data.nc')
+            pass
+        
+               
+
 
     def get_xarray(self):
         """
@@ -291,7 +328,7 @@ class WeatherStore(WeatherStore_):
         :returns ds: xarray dataset containing user-selected variables (e.g., merged aCCFs, mean aCCFs, Climate hotspots).
         :rtype: dataset
         """
-        return xr.Dataset(self.var_xr, self.axes)
+        return self.ds_xr
 
     def reduce_domain(self, bounds, verbose=False):
         """